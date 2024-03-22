7d09f4d38175aab7c40d7605c696920a9bfc61ed
ziyao <2144189170@qq.com>
2024-03-22 21:11:14 +0800

    add fill dialog

---
+++ dialog_fill_xlsx.py
@@ -0,0 +1,9 @@
+
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QDialog, QWidget
+
+
+class dialog_fill_xlsx(QDialog):
+    def __init__(self, parent: QWidget | None = ..., flags: Qt.WindowFlags | Qt.WindowType = ...) -> None:
+        super().__init__(parent, flags)
+
